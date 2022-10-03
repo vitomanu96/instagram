568f724c3d272f14bbd41fb6a5854b996a446b5e
Vito Manuguerra <vito.manuguerra@soft.it>
2022-10-03 17:55:35 +0200

    docs folder

---
+++ docs/posts.js
@@ -0,0 +1,13 @@
+const posts = [{
+	userImage: '',
+	userName: '',
+	postImage: '',
+	like: true,
+	comments: [ {
+		userImage: '',
+		userName: '',
+		text: ''
+	}],
+	likes: ['vitomanu','giacomobaz'],
+	createDate: '1664812347283'
+}]
