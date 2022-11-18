0555280a4c73d59d24e40833c0e7ce06ce4ef922
Vito Manuguerra <vito.manuguerra@soft.it>
2022-11-18 11:29:33 +0100

    revisione codice

--- docs/posts.js
+++ docs/posts.js
@@ -1,43 +1,45 @@
-const posts = [{
-	userImage: './imgs/gundam logo.jpg',
-	userName: 'Giacomo Bazzano',
-	postImage: './imgs/Mobile_Suit_Gundam.jpg',
-	like: true,
-	comments: [ {
-		userImageC: './imgs/UsrImgBot1.png',
-		userNameC: 'Bot1',
-		text: 'Nice one'
-	}],
-	likes: ['vitomanu','giacomobaz'],
-	createDate: '1664812347283'
-},
-
-{
-	userImage2: './imgs/2.jpg',
-	userName2: 'Post Bot 2',
-	postImage2: './imgs/woods.jpg',
-	like2: true,
-	comments2: [ {
-		userImageC2: './imgs/Bot 2.png',
-		userNameC2: 'Bot2',
-		text2: 'Nice one'
-	}],
-	likes: ['vitomanu','giacomobaz'],
-	createDate: '1664812347283'
-},
-
-{
-	userImage3: './imgs/3.jpg',
-	userName3: 'Post Bot 3',
-	postImage3: './imgs/snowoods.jpg',
-	like3: true,
-	comments3: [ {
-		userImageC3: './imgs/Bot 3.png',
-		userNameC3: 'Bot3',
-		text3: 'Nice one'
-	}],
-	likes: ['vitomanu','giacomobaz'],
-	createDate: '1664812347283'
-}
-];
+const posts = [
+    {
+        id: 1,
+        userImage: './imgs/gundam logo.jpg',
+        userName: 'Giacomo Bazzano',
+        postImage: './imgs/Mobile_Suit_Gundam.jpg',
+        like: true,
+        comments: [{
+            userImageC: './imgs/UsrImgBot1.png',
+            userNameC: 'Bot1',
+            text: 'Nice one'
+        }],
+        likes: ['vitomanu', 'giacomobaz'],
+        createDate: '1664812347283'
+    },
+    {
+        id: 2,
+        userImage: './imgs/2.jpg',
+        userName: 'Post Bot 2',
+        postImage: './imgs/woods.jpg',
+        like: false,
+        comments2: [{
+            userImageC: './imgs/Bot 2.png',
+            userNameC: 'Bot2',
+            text2: 'Nice one'
+        }],
+        likes: ['vitomanu', 'giacomobaz'],
+        createDate: '1664812347283'
+    },
+    {
+        id: 3,
+        userImage: './imgs/3.jpg',
+        userName: 'Post Bot 3',
+        postImage: './imgs/snowoods.jpg',
+        like: true,
+        comments: [{
+            userImageC: './imgs/Bot 3.png',
+            userNameC: 'Bot3',
+            text3: 'Nice one'
+        }],
+        likes: ['vitomanu', 'giacomobaz'],
+        createDate: '1664812347283'
+    }
+]
 
--- main.js
+++ main.js
@@ -1,18 +1,48 @@
+const posts = [
+    {
+        id: 1,
+        userImage: './imgs/gundam logo.jpg',
+        userName: 'Giacomo Bazzano',
+        postImage: './imgs/Mobile_Suit_Gundam.jpg',
+        like: true,
+        comments: [{
+            userImageC: './imgs/UsrImgBot1.png',
+            userNameC: 'Bot1',
+            text: 'Nice one'
+        }],
+        likes: ['vitomanu', 'giacomobaz'],
+        createDate: '1664812347283'
+    },
+    {
+        id: 2,
+        userImage: './imgs/2.jpg',
+        userName: 'Post Bot 2',
+        postImage: './imgs/woods.jpg',
+        like: false,
+        comments2: [{
+            userImageC: './imgs/Bot 2.png',
+            userNameC: 'Bot2',
+            text2: 'Nice one'
+        }],
+        likes: ['vitomanu', 'giacomobaz'],
+        createDate: '1664812347283'
+    },
+    {
+        id: 3,
+        userImage: './imgs/3.jpg',
+        userName: 'Post Bot 3',
+        postImage: './imgs/snowoods.jpg',
+        like: true,
+        comments: [{
+            userImageC: './imgs/Bot 3.png',
+            userNameC: 'Bot3',
+            text3: 'Nice one'
+        }],
+        likes: ['vitomanu', 'giacomobaz'],
+        createDate: '1664812347283'
+    }
+]
 
-
-const post = {
-    userImage: "./imgs/gundam logo.jpg",
-    userName: 'Vito Manuguerra',
-    postImage:'./imgs/Mobile_Suit_Gundam.jpg',
-    like: true,
-    comments: [{
-        userImage: './img/UsrImgBot1.jpg',
-        userName: 'bot1',
-        text: 'nice img, love it'
-    }],
-    likes: ['vitomanu', 'giacomobaz'],
-    createDate: '1664812347283'
-}
 
 // @giaco interazione con il like svg
 function handleClickLike() {
@@ -22,34 +52,64 @@ function handleClickLike() {
     likeSvg.style.fill = post.like ? '#FF0000' : '#000000'
 }
 
-const handleClickLikeBind = () => {
-    // toogle like
-    post.like = !post.like // !true
-    const likeSvg = document.getElementById('like')
-    likeSvg.style.fill = post.like ? '#FF0000' : '#000000'
-}
-
-
+// esercizio 1.1
+// 1.2 aggiungere la logica del like
 window.addEventListener('load', (event) => {
-    console.log('page is fully loaded', new Date().getTime())
-    console.log('Start instagram')
 
-    const usernameDiv = document.getElementById('username')
-    const likeSvg = document.getElementById('like')
-    const imgProfile = document.getElementById('imgProfile')
-    console.log(usernameDiv, new Date().getTime())
-    console.log('likeSvg', likeSvg)
-    usernameDiv.textContent = post.userName
-    imgProfile.src = post.userImage
+    const article = document.createElement('article')
+    article.className = 'article-post'
+    article.id = 1
 
-    likeSvg.style.fill = post.like ? '#FF0000' : '#000000'
-/*
-    if (post.like) {
-        likeSvg.color = 'red'
-    } else {
-        likeSvg.color = 'black'
-    }*/
+    const headerPost = document.createElement('div')
+    headerPost.className = 'header-post'
+
+    const userNameElem = document.createElement('h4')
+    userNameElem.textContent = 'Vito'
+
+    /*TODO 1. aggiungere anche body-post e footer-post*/
+
+    headerPost.appendChild(userNameElem)
+
+    article.appendChild(headerPost)
+
+    const container = document.getElementById('posts-container')
+
+    container.appendChild(article)
+
 })
+
+// esercizio 2.1: creare lista di posts
+// 2.2 aggiungere il like
+// 2.3 il medesimo esercizio potrebbe essere fatto utilizzando il "map" al posto del foreach
+// 2.3 NOTA container.append([article, article, article, article])
+/*
+window.addEventListener('load', (event) => {
+
+    const container = document.getElementById('posts-container')
+
+    posts.forEach((post, index) => {
+
+        console.log('post ', post, ' con indice ', index)
+        const article = document.createElement('article')
+        article.className = 'article-post'
+        article.id = post.id
+
+        const headerPost = document.createElement('div')
+        headerPost.className = 'header-post'
+
+        const userNameElem = document.createElement('h4')
+        userNameElem.textContent = post.userName
+
+        /!*TODO 1. aggiungere anche body-post e footer-post*!/
+
+        headerPost.appendChild(userNameElem)
+
+        article.appendChild(headerPost)
+
+        container.appendChild(article)
+    })
+})
+*/
 
 
 
